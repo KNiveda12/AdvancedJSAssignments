9b364549159ace83c67b5fee0e2d226c34505f7e
KNiveda12 <nivedavarma1296@gmail.com>
2025-11-24 21:57:53 +0530

    changes done

---
+++ Day10/Q16/Q16a.js
@@ -0,0 +1,5 @@
+const isEven = (n) => {
+   return (n%2==0)?true:false;
+}
+console.log(isEven(12));
+console.log(isEven(5));
---
+++ Day10/Q16/Q16b.js
@@ -0,0 +1,5 @@
+const result = (marks) => {
+  return (marks>=35)?"Pass":"Fail";
+}
+console.log(result(66));
+console.log(result(22));
